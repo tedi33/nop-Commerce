80dad7db886c7ec850101f65311f62c8c0879a46
tedi99 <boss6a12@abv.bg>
2014-12-08 16:54:13 +0200

    tedi

---
+++ Plugins/SevenSpikes.Nop.Plugins.Attachments/Upgrade/7Spikes_readme.txt
@@ -0,0 +1,7 @@
+﻿Steps:
+1. Backup your existing database
+2. If you haven't upgraded your nopCommerce website to version 3.4 already, then follow the instructions in the "nopCommerce_readme.txt".
+3. Copy this plugin and the "SevenSpikes.Core" plugin folders to the Plugins folder of nopCommerce.
+4. Start your site. If you have made any changes to this plugin in 3.3 then you need to merge them in the new version of the plugin.
+
+Note: If you have Nop Ajax Filters plugin installed then you need to uninstall and then install it again.
---
+++ Plugins/SevenSpikes.Nop.Plugins.SalesCampaigns/Scripts/SaleCampaigns.js
@@ -0,0 +1,55 @@
+﻿function formatTime(element, timeInSeconds, clockType) {
+    var days_remaining = Math.floor(timeInSeconds / 60 / 60 / 24);
+    var hours_remaining = timeInSeconds / 60 / 60;
+
+    if (clockType !== 10) {
+        hours_remaining = hours_remaining % 24;
+    }
+
+    hours_remaining = Math.floor(hours_remaining);
+
+    var minutes_remaining = Math.floor((timeInSeconds / 60) % 60);
+    var seconds_remaining = timeInSeconds % 60;
+
+    var timeString = "";
+
+    if (clockType !== 10) {
+        timeString += days_remaining + " " + (days_remaining !== 1 ? localizationArray['days'] : localizationArray['day']);
+    }
+
+    if (clockType !== 15) {
+        if (clockType !== 10) {
+            timeString += ", ";
+        }
+
+        timeString += hours_remaining + " " + (hours_remaining !== 1 ? localizationArray['hours'] : localizationArray['hour']) + ", ";
+        timeString += minutes_remaining + " " + (minutes_remaining !== 1 ? localizationArray['minutes'] : localizationArray['minute']) + ", ";
+        timeString += seconds_remaining + " " + (seconds_remaining !== 1 ? localizationArray['seconds'] : localizationArray['second']);
+    }
+
+    $(element).text(timeString);
+}
+
+$(document).ready(function () {
+    $('.countdownTimer').each(function () {
+        var current = $(this);
+        var time = current.data("time");
+        var seconds = parseInt(time);
+
+        var type = current.data("clocktype");
+        var clockType = parseInt(type);
+
+        var interval = setInterval(function () {
+            formatTime(current, seconds, clockType);
+
+            seconds -= 1;
+
+            current.data("time", seconds);
+
+            if (seconds < 0) {
+                $(current).parent().remove();
+                clearInterval(interval);
+            }
+        }, 1000);
+    });
+});
